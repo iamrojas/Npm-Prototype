f43647a79b64b0ff21c11b0ec024541b21c77e63
Michael Rojas <mrojas@Michaels-iMac.local>
2019-01-21 16:40:26 -0500

    community build

--- js/main.js
+++ js/main.js
@@ -1,134 +1,86 @@
-var swiper = new Swiper('.featured .swiper-container', {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  loop: true,
-  effect: 'fade',
-  speed:2000,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  fadeEffect: {
-    crossFade: true
-  },
-   autoplay: {
-    delay: 5000,
-  },
-});
+{
+    // Test Animation Class
+    class Animation {
+        constructor(el){
+            el = document.querySelector(el);
+            this.DOM = {el: el};
+        }
+        
+        fadeDown(target, context){
+            let introAnime = new Waypoint({
+                element: this.DOM.el,
+                handler: function(direction) {
+                    anime({
+                        targets: target,
+                        opacity: [0, 1],
+                        translateY: [-50, 0],
+                        duration: 2000,
+                        easing: 'easeInOutQuad',
+                        delay: function(el, index) {
+                            return index * 800;
+                        },
+                    })
+                },
+                context: context,
+                offset: 1500
+            });
+        }
 
-
-
-
-$('a.to-content').click(function(){
-    $('html, body').animate({
-      scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-  });
-
-
-$(".header__menu-toggle").click(function(e){
-  $('body').toggleClass('show');
- 
-});
-
-$('.menu-overlay').click(function(){
-  $('body').toggleClass('show');
-});
-
-
-$('.nav-main').click(function(){
-  $('body').toggleClass('show');
-});
-
-$('.nav-menu li a').hover(
-    function(){
-        //we get our current filename and use it for the src
-        var linkIndex = $(this).attr("data-filename");
-        $(this).addClass('hover');
-        $('.nav__img img').attr('src', 'images/'+linkIndex+'.jpg');
-    },
-    function(){
-        $(this).removeClass('hover');
-        $('.nav__img img').attr('src', 'images/image1.jpg');
+        fadeLeft(target, context){
+            let introAnime = new Waypoint({
+                element: this.DOM.el,
+                handler: function(direction) {
+                    anime({
+                        targets: target,
+                        opacity: [0, 1],
+                        translateX: [-50, 0],
+                        duration: 1000,
+                        easing: 'easeOutQuad',
+                        delay: function(el, index) {
+                            return index * 300;
+                        },
+                    })
+                },
+                context: context,
+                offset: 1500
+            });
+        }
     }
-);
 
+    //Initialize the animations
 
-var controller = new ScrollMagic.Controller();
+    // const heroAnimation= new Animation('.hero');
+    // heroAnimation.fadeLeft([".hero__headline" , ".hero__button"]);
 
-//Hero
-var headline1 =  $('.headline'),
-  heroShape =  $('.hero__shape.shape--left'),
-  heroDown =  $('.hero__down'),
-  heroTl = new TimelineMax();
-
-  heroTl
-    .from(headline1, 2, { autoAlpha:0, y:-50, opacity:0, ease:Back.easeOut})
-    .from(heroShape, 3, { autoAlpha:0, x:-50, opacity:0, ease:Power3.easeOut} , '-=1')
-    .from(heroDown, 1, { autoAlpha:0, y:-150, opacity:0, ease:Sine.easeOut}  , '-=2');
     
-  var scene = new ScrollMagic.Scene({triggerElement: '.hero'})
-    .setTween(heroTl)
-    .addTo(controller);
+    //Owl Carousel Options
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:1
+    });
 
-  heroDown.hover(
-     function() {
-        TweenLite.to($(this), 0.04, {ease: Elastic.easeOut.config(1, 0.3),y:-5});
-     },
-     function() {
-        TweenLite.to($(this), 0.04, {ease: Elastic.easeOut.config(1, 0.3), y:0});  
-     }
-  );
-
-//Services
-var serviceImg =  $('.services__img-c'),
-    heroShapeRight =  $('.hero__shape.shape--right'),
-    serviceCol =  $('.services__col'),
-  serviceTl = new TimelineMax();
-
-  serviceTl
-    .from(serviceCol, 2, { autoAlpha:0, y:-150, opacity:0, ease:Back.easeOut} )
-    .from(serviceImg, 2, { autoAlpha:0, y:-150, opacity:0, ease:Back.easeOut} , '-=1.8')
-     .from(heroShapeRight, 2, { autoAlpha:0, x:200, opacity:0, ease:Power3.easeOut} , '-=2');
-    
-  var scene = new ScrollMagic.Scene({triggerElement: '.services', offset: -200})
-    .setTween(serviceTl)
-    .addTo(controller);
-
-//Services
-var featuredTitle =  $('.featured__title'),
-    featured =  $('.featured'),
-  featuredTl = new TimelineMax();
-
-  featuredTl
-    .from(featuredTitle, 2, { autoAlpha:0, y:-150, opacity:0, ease:Back.easeOut} )
-    .from(featured, 2, { autoAlpha:0, y:-150, opacity:0, ease:Back.easeOut} , '-=2');
-    
-  var scene = new ScrollMagic.Scene({triggerElement: '.featured__col' , offset: -600})
-    .setTween(featuredTl)
-    .addTo(controller);
-
-
-//About
-var aboutTitle =  $('.about__title'),
-    aboutImg =  $('.about__img-c'),
-    aboutContent =  $('.about__content'),
-  aboutTl = new TimelineMax();
-
-  aboutTl
-    .from(aboutTitle, 2, { autoAlpha:0, y:-150, opacity:0, ease:Back.easeOut} )
-    .from(aboutImg, 2, { autoAlpha:0, y:-150, opacity:0, ease:Back.easeOut}, '-=2' )
-    .from(aboutContent, 2, { autoAlpha:0, y:-150, opacity:0, ease:Back.easeOut}, '-=2' );
-    
-  var scene = new ScrollMagic.Scene({triggerElement: '.about' , offset: -600})
-    .setTween(aboutTl)
-    .addTo(controller);
-
-
-
-
+    // Bulma Toggle Nav
+    document.addEventListener('DOMContentLoaded', () => {
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+        
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+        
+            // Add a click event on each of them
+            $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+        
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+        
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+        
+            });
+            });
+        }
+    });
+}
